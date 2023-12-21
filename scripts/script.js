04b3676d8222f70c3b0aac742dd91ad16f549bb6
JordanS <jordy7789@hotmail.com>
2023-12-20 23:52:00 -0800

    Add operate function

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
+const ADD = 0;
+const SUBTRACT = 1;
+const MULTIPLY = 2;
+const DIVIDE = 3;
+
 function add(a, b) {
     return a + b;
 }
@@ -14,3 +19,24 @@ function divide(a, b) {
     return a/b;
 }
 
+function operate(a, op, b) {
+    switch(op) {
+        case ADD:
+            return add(a,b);
+            break;
+        case SUBTRACT:
+            return subtract(a,b);
+            break;
+        case MULTIPLY:
+            return multiply(a,b);
+            break;
+        case DIVIDE:
+            return divide(a,b);
+            break;
+        default:
+    }
+}
+
+function calculator(a, op, b) {
+
+}
